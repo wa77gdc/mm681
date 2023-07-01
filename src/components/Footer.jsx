c4316596c6dc28da2f8c4212e795ed2a4ec34de2
wa77gdc <wa77gdc@gmail.com>
2023-07-01 08:28:19 +0700

    1_7_1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ const Footer = () => {
   return (
     <div className='Footer'>
       <p className="font08-footer">
-        2022 Copyight @
-        <span className='color-wed-footer'> MM68APP.COM</span>
+        2023 Copyight @
+        <span className='color-wed-footer'> PPF4APP.COM</span>
       </p>
       <p>ALL RIGHTS RESERVED</p>
     </div>
